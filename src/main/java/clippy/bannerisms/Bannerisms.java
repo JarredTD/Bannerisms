a98a9e0f0ab6fc7ca0659c348d3892936867a915
JarredTD <jarred.desrosiers@yahoo.com>
2023-07-12 17:52:17 -0700

    init

---
+++ src/main/java/clippy/bannerisms/Bannerisms.java
@@ -0,0 +1,17 @@
+package clippy.bannerisms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bannerisms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
